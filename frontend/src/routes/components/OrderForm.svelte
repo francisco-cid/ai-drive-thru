2965c0611c676012ef2fff6bc4ac7ae09ea7ed45
francisco-cid <60446619+francisco-cid@users.noreply.github.com>
2025-02-05 10:05:02 -0600

    wrap submission component in form

--- frontend/src/routes/components/OrderForm.svelte
+++ frontend/src/routes/components/OrderForm.svelte
@@ -7,14 +7,15 @@
     export let onSubmit: () => Promise<void>;
 </script>
 
-<div class="flex items-center gap-4">
+<form on:submit|preventDefault={onSubmit} class="flex items-center gap-4">
     <Input
         bind:value={orderMessage}
         class="flex-1 p-4 h-20 text-lg"
         placeholder="Ex: &quot;I would like 1 burger & fries&quot;"
     />
     <Button
+        type="submit"
         class="w-16 h-16 rounded-full text-lg flex items-center justify-center bg-blue-600 hover:bg-blue-700"
         on:click={onSubmit}>Run</Button
     >
-</div>
+</form>
