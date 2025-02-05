5f7954e7b46fe252619d25c7b5ae2c0c091d9f1d
francisco-cid <60446619+francisco-cid@users.noreply.github.com>
2025-02-05 10:20:12 -0600

    add loading state

--- frontend/src/routes/components/OrderForm.svelte
+++ frontend/src/routes/components/OrderForm.svelte
@@ -1,21 +1,38 @@
 <script lang="ts">
     import { Input } from "$lib/components/ui/input";
     import { Button } from "$lib/components/ui/button";
+    import { Loader } from "lucide-svelte";
 
     // receive props
     export let orderMessage: string;
     export let onSubmit: () => Promise<void>;
+
+    let isLoading: boolean = false;
+
+    const handleSubmit = async () => {
+        isLoading = true;
+        await onSubmit();
+        isLoading = false;
+    };
 </script>
 
-<form on:submit|preventDefault={onSubmit} class="flex items-center gap-4">
+<form on:submit|preventDefault={handleSubmit} class="flex items-center gap-4">
     <Input
         bind:value={orderMessage}
         class="flex-1 p-4 h-20 text-lg"
         placeholder="Ex: &quot;I would like 1 burger & fries&quot;"
+        disabled={isLoading}
     />
     <Button
         type="submit"
         class="w-16 h-16 rounded-full text-lg flex items-center justify-center bg-blue-600 hover:bg-blue-700"
-        on:click={onSubmit}>Run</Button
+        on:click={onSubmit}
+        disabled={isLoading}
     >
+        {#if isLoading}
+            <Loader class="animate-spin" size={24} />
+        {:else}
+            Run
+        {/if}
+    </Button>
 </form>
